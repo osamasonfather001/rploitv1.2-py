c355ec06a6ebba2181466d03dce91700b3c8140e
osamasonfather001 <meykoloves@gmail.com>
2025-02-17 02:05:12 -0500

    Update functions.bat

--- utils/functions.bat
+++ utils/functions.bat
@@ -1,1 +1,24 @@
+@echo off
+setlocal
 
+set "a=RobloxPlayerBeta.exe"
+
+:b
+tasklist /FI "IMAGENAME eq %a%" | find /I "%a%" >nul
+if %ERRORLEVEL%==0 (
+    goto c
+) else (
+    goto d
+)
+
+:c
+taskkill /IM "%a%" /F
+timeout /t 2 >nul
+goto d
+
+:d
+start "" "C:\Path\To\RobloxPlayerBeta.exe"
+goto e
+
+:e
+exit
