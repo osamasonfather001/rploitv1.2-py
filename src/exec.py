2720e2b74fcf9c08d6bdcf5486773f02f4411edf
osamasonfather001 <meykoloves@gmail.com>
2025-02-17 01:40:40 -0500

    Update exec.py

--- src/exec.py
+++ src/exec.py
@@ -1,1 +1,18 @@
+# src/exec.py
 
+import os
+import sys
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from luac import get_server_count
+
+if __name__ == "__main__":
+    game_id = input("enter script (gameid)")
+    server_count = get_server_count(game_id)
+    
+    if server_count is not None:
+        print(f"Number of servers for game ID {game_id}: {server_count}")
+    else:
+        print("Failed to retrieve server count.")
